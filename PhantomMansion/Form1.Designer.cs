32c677d283d4b0ce47b3875cca1655774e6dd779
WeaponOwl <misha@serverdev.net>
2017-11-16 22:53:32 +0200

    initial commit

---
+++ PhantomMansion/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace PhantomMansion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Light Blue",
+            "Blue",
+            "Purple"});
+            this.listBox1.Location = new System.Drawing.Point(777, 15);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 132);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(777, 153);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(139, 500);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(759, 642);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AutoScroll = true;
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(751, 613);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "TileSet";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(751, 613);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Map";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 288);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(325, 288);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.richTextBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(751, 613);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "RawTiles";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.richTextBox2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(751, 613);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "RawItems";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.richTextBox3);
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(751, 613);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "RawObjects";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(745, 607);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(751, 613);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox3.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(751, 613);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.richTextBox4);
+            this.tabPage6.Location = new System.Drawing.Point(4, 25);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(751, 613);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "RawMaps";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox4.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(751, 613);
+            this.richTextBox4.TabIndex = 0;
+            this.richTextBox4.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 666);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+    }
+}
+
---
+++ PhantomMansion/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+
+namespace PhantomMansion
+{
+    public partial class Form1 : Form
+    {
+        private string[] basicUrls = new string[]{
+            "http://www8.agame.com/mirror/flash/p/phantommansionred",
+            "http://www8.agame.com/mirror/flash/p/phantommansionorange",
+            "http://www8.agame.com/mirror/flash/p/phantommansionyellow",
+            "http://www8.agame.com/mirror/flash/p/phantommansiongreen",
+            "http://www8.agame.com/mirror/flash/p/phantommansionblue",
+            "http://www8.agame.com/mirror/flash/p/phantommansionindigo",
+            "http://www8.agame.com/mirror/flash/p/phantommansionviolet"
+        };
+
+        private XmlDocument mapsInfo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var nodes = mapsInfo.SelectNodes("/project/layergroup");
+            var lobby = nodes[listBox2.SelectedIndex];
+
+            // Tiles
+            var tilelayer = lobby.SelectNodes("./tilelayer")[1];
+            string tilePath = tilelayer.Attributes["xmlFile"].Value;
+
+            var width = int.Parse(tilelayer.SelectSingleNode("./tilemap/width").InnerText);
+            var height = int.Parse(tilelayer.SelectSingleNode("./tilemap/height").InnerText);
+
+            string layerUrl = basicUrls[listBox1.SelectedIndex] + "/_maps" + tilePath.Replace('\\', '/');
+            string layerString = "";
+
+            using (WebClient client = new WebClient())
+                layerString = client.DownloadString(layerUrl);
+            richTextBox1.Text = layerString;
+
+            XmlDocument layerInfo = new XmlDocument();
+            layerInfo.LoadXml(layerString);
+
+            string layerBody = layerInfo.SelectSingleNode("./tiledLayer/data").Attributes["tiledata"].Value;
+
+            // Items
+            var itemlayer = lobby.SelectNodes("./tilelayer")[0];
+            string itemPath = itemlayer.Attributes["xmlFile"].Value;
+
+            layerUrl = basicUrls[listBox1.SelectedIndex] + "/_maps" + itemPath.Replace('\\', '/');
+            layerString = "";
+
+            using (WebClient client = new WebClient())
+                layerString = client.DownloadString(layerUrl);
+            richTextBox2.Text = layerString;
+
+            layerInfo = new XmlDocument();
+            layerInfo.LoadXml(layerString);
+
+            string itemLayerBody = layerInfo.SelectSingleNode("./tiledLayer/data").Attributes["tiledata"].Value;
+
+            // Objects
+            var objlayer = lobby.SelectSingleNode("./objectlayer");
+            string objPath = objlayer.Attributes["xmlFile"].Value;
+
+            layerUrl = basicUrls[listBox1.SelectedIndex] + "/_maps" + objPath.Replace('\\', '/');
+            layerString = "";
+            using (WebClient client = new WebClient())
+                layerString = client.DownloadString(layerUrl);
+            richTextBox3.Text = layerString;
+
+            layerInfo = new XmlDocument();
+            layerInfo.LoadXml(layerString);
+
+            var objNodes = layerInfo.SelectNodes("/objectLayer/data/object");
+
+            // Render
+            Bitmap tileSet = null;
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(basicUrls[listBox1.SelectedIndex] + "/_tiles/tiles.png");
+
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    tileSet = new Bitmap(Bitmap.FromStream(mem));
+                }
+            }
+
+            Bitmap mapSet = new Bitmap(width * 32, height * 32);
+
+            using (Graphics grD = Graphics.FromImage(mapSet))
+            {
+                string[] tileCells = layerBody.Split(',');
+                string[] itemCells = itemLayerBody.Split(',');
+                for (int i = 0; i < tileCells.Length; i++)
+                {
+                    int tileid = int.Parse(tileCells[i]);
+                    int itemid = int.Parse(itemCells[i]);
+
+                    grD.DrawImage(tileSet, new Rectangle((i / height) * 32, (i % height) * 32, 32, 32), new Rectangle((tileid % 4) * 32, (tileid / 4) * 32, 32, 32), GraphicsUnit.Pixel);
+                    if (itemid > 0)
+                    {
+                        grD.DrawString(itemid.ToString(), System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, (i / height) * 32 + 7, (i % height) * 32 + 7);
+                        grD.DrawString(itemid.ToString(), System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, (i / height) * 32 + 5, (i % height) * 32 + 5);
+                        grD.DrawString(itemid.ToString(), System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, (i / height) * 32 + 5, (i % height) * 32 + 7);
+                        grD.DrawString(itemid.ToString(), System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, (i / height) * 32 + 7, (i % height) * 32 + 5);
+                        grD.DrawString(itemid.ToString(), System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.White, (i / height) * 32 + 6, (i % height) * 32 + 6);
+                    }
+                }
+
+                foreach (XmlNode node in objNodes)
+                {
+                    int x = int.Parse(node.Attributes["x"].Value);
+                    int y = int.Parse(node.Attributes["y"].Value);
+
+                    string value = node.Attributes["value"].Value;
+
+                    grD.DrawString(value, System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, x * 32 + 7, y * 32 + 7);
+                    grD.DrawString(value, System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, x * 32 + 5, y * 32 + 7);
+                    grD.DrawString(value, System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, x * 32 + 5, y * 32 + 5);
+                    grD.DrawString(value, System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Black, x * 32 + 7, y * 32 + 5);
+                    grD.DrawString(value, System.Drawing.SystemFonts.CaptionFont, System.Drawing.Brushes.Yellow, x * 32 + 6, y * 32 + 6);
+                }
+            }
+
+            pictureBox1.Image = mapSet;
+            pictureBox1.Width = mapSet.Width;
+            pictureBox1.Height = mapSet.Height;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string mapsUrl = basicUrls[listBox1.SelectedIndex] + "/_maps/deluxe.xml";
+            string mapsData = "";
+
+            using (WebClient client = new WebClient())
+                mapsData = client.DownloadString(mapsUrl);
+            richTextBox4.Text = mapsData;
+
+            mapsInfo = new XmlDocument();
+            mapsInfo.LoadXml(mapsData);
+
+            var nodes = mapsInfo.SelectNodes("/project/layergroup");
+            listBox2.Items.Clear();
+            foreach (XmlNode node in nodes)
+            {
+                var nameNode = node.SelectSingleNode("./name");
+                listBox2.Items.Add(nameNode.InnerText);
+            }
+            listBox2.SelectedIndex = 0;
+
+            using (WebClient webClient = new WebClient())
+            {
+                byte[] data = webClient.DownloadData(basicUrls[listBox1.SelectedIndex] + "/_tiles/tiles.png");
+
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    pictureBox2.Image = new Bitmap(Bitmap.FromStream(mem));
+                }
+            }
+
+        }
+    }
+}
